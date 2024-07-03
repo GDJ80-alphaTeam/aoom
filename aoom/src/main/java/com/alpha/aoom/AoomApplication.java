645209a5cf9300675658eb3ba5a86969698dee3e
leeDragonHun <9leeyh5@gmail.com>
2024-07-03 14:19:49 +0900

    AOOM 프로젝트 초기화

---
+++ aoom/src/main/java/com/alpha/aoom/AoomApplication.java
@@ -0,0 +1,13 @@
+package com.alpha.aoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AoomApplication.class, args);
+	}
+
+}
---
+++ aoom/src/test/java/com/alpha/aoom/AoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha.aoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
