d90c219aba52c3d434dec5e0cb8bea1527c275b3
tmdduq500 <osy9907@gmail.com>
2024-07-10 20:37:39 +0900

    [오승엽] 2024.07.10 commit 1 : HostController 작업 중
    [추가]
    1. 호스트모드 main, 호스트모드 숙소관리 페이지 호출하는 controller추가
    2. 호스트모드 main view에 숙소관리 a태그 추가
    3. 숙소 추가 버튼 클릭하면 ROOM테이블에 숙소 생성하는 ajax 작업 중

--- aoom/src/main/java/com/alpha/aoom/host/hostController/HostController.java
+++ aoom/src/main/java/com/alpha/aoom/host/hostController/HostController.java
@@ -1,8 +1,21 @@
 package com.alpha.aoom.host.hostController;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/host")
 public class HostController {
 
+	// 호스트 모드 메인화면 호출
+	@RequestMapping("/main")
+	public String main() {
+		return "/host/main";
+	}
+	
+	// 호스트 모드 숙소 관리 화면 호출
+	@RequestMapping("/roomManage")
+	public String roomManagement() {
+		return "/host/roomManage";
+	}
 }
