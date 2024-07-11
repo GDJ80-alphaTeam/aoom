2be8e899eceb2c773c2e64161e1a55051e1e19e6
leeDragonHun <9leeyh5@gmail.com>
2024-07-11 10:25:10 +0900

    [이용훈] 2024.07.11 commit 2 : modelMap, sql조각 사용 방식으로 변경 2
    [수정]
    1. model 방식에서 modelMap방식으로 변경
    2. 쿼리문에 select에 *로 전체 출력하는 방식에서
       -> sql조각을 사용하여 include하는 방식으로 변경(범용성)
    3. Main.java를 MainContoller로 새로 만듬(리네임 x)

--- aoom/src/main/java/com/alpha/aoom/main/controller/MainController.java
+++ aoom/src/main/java/com/alpha/aoom/main/controller/MainController.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.alpha.aoom.room.service.RoomMapper;
@@ -15,20 +15,20 @@ import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
-public class Main {
+public class MainController {
 	
 	@Autowired
 	RoomMapper roomMapper;
 	
 	// 메인페이지 호출
 	@RequestMapping("/main")
-	public String main(HttpSession session, Model m) {
+	public String main(HttpSession session, ModelMap modelMap) {
 		
 		// 숙소 전체 목록 조회
 		List<Map<String, Object>> retrieveList = roomMapper.retrieveList();
 		
 		// model에 숙소 전체 목록 조회한 값 넣기
-		m.addAttribute("roomAllList", retrieveList);		
+		modelMap.addAttribute("roomAllList", retrieveList);		
 		
 		return "main";
 	}
